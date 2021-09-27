aaa18325f1f2e50535b8558db999630bc467dcaa
Graham Ollis <plicease@cpan.org>
2021-09-27 17:14:12 -0600

    Add common dynamic deps

--- lib/Task/AlienDev.pm
+++ lib/Task/AlienDev.pm
@@ -43,6 +43,27 @@ Some useful L<Dist::Zilla> plugins useful for developing L<Alien::Build> based L
 The latest versions as of when this L<Task> was released should be installed at minimum if
 they are not already installed.
 
+In addition these modules, which are dynamic dependencies on some platforms, are installed:
+
+=over 4
+
+=item L<Env::ShellWords>
+
+=item L<File::Listing>
+
+=item L<HTTP::Tiny>
+
+=item L<Mojo::DOM58>
+
+=item L<Sort::Versions>
+
+=item L<URI>
+
+=back
+
+Having these dynamic dependencies pre-installed makes it easier to test L<Alien>s in both
+C<share> and C<system> modes.
+
 Other prereqs may be added in the future if they are deemed useful for L<Alien> development.
 
 =head1 CAVEATS
